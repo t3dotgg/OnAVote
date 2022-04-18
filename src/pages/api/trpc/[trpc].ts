3647688551e995243c54a2923bdb75d1621ee32c
theo <me@t3.gg>
2022-04-17 19:06:16 -0700

    Working trpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,18 +2,27 @@ import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { z } from "zod";
 
-export const appRouter = trpc.router().query("hi", {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      greeting: `hello ${input?.text ?? "world"}`,
-    };
-  },
-});
+import { prisma } from "../../../db/client";
+
+export const appRouter = trpc
+  .router()
+  .query("hi", {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      return {
+        greeting: `hello ${input?.text ?? "world"}`,
+      };
+    },
+  })
+  .query("getAllQuestions", {
+    async resolve() {
+      return await prisma.pollQuestion.findMany();
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,11 @@
-import Head from "next/head";
-
-import { prisma } from "../db/client";
 import { trpc } from "../utils/trpc";
 
 export default function Home(props: any) {
-  const { data, isLoading } = trpc.useQuery(["hi"]);
+  const { data, isLoading } = trpc.useQuery(["getAllQuestions"]);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
-  return <div>{data.greeting}</div>;
+  console.log(data);
+
+  return <div>{data[0]?.question}</div>;
 }
-
-export const getServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany();
-
-  return {
-    props: {
-      questions: JSON.stringify(questions),
-    },
-  };
-};
