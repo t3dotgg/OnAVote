148fd2ce449c4f84d490da85743ce9ae4f6ca0d4
theo <me@t3.gg>
2022-04-17 19:30:51 -0700

    Ability to add questions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
+import React from "react";
 import { trpc } from "../utils/trpc";
 
 const QuestionCreator: React.FC = () => {
-  const { mutate } = trpc.useMutation("questions.create");
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const client = trpc.useContext();
+  const { mutate, isLoading } = trpc.useMutation("questions.create", {
+    onSuccess: (data) => {
+      console.log("did we succeed?", data);
+      client.invalidateQueries(["questions.get-all"]);
+      if (!inputRef.current) return;
+      inputRef.current.value = "";
+    },
+  });
 
   return (
     <input
-      onSubmit={(event) => {
-        console.log("value?", event.currentTarget.value);
+      ref={inputRef}
+      disabled={isLoading}
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          console.log("enter!!!", event.currentTarget.value);
+          mutate({ question: event.currentTarget.value });
+        }
       }}
     ></input>
   );
@@ -18,10 +33,16 @@ export default function Home() {
   if (isLoading || !data) return <div>Loading...</div>;
 
   return (
-    <div>
+    <div className="p-6 flex flex-col">
       <div className="flex flex-col">
         <div className="text-2xl font-bold">Questions</div>
-        {data[0]?.question}
+        {data.map((question) => {
+          return (
+            <div key={question.id} className="my-2">
+              {question.question}
+            </div>
+          );
+        })}
       </div>
       <QuestionCreator />
     </div>
