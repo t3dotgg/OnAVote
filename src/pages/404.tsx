dadc23a847396dd93de7fb168e95a279be0d6be7
Theo Browne <me@t3.gg>
2022-07-12 11:17:22 -0700

    kill for now :(

--- src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="antialiased min-h-screen flex items-center justify-center text-xl font-bold">
-      404 | Not Found
-    </div>
-  );
-}
--- src/pages/api/trpc/[trpc].ts
+++
@@ -1,9 +0,0 @@
-import * as trpcNext from "@trpc/server/adapters/next";
-import { appRouter } from "../../../backend/router";
-import { createContext } from "../../../backend/router/context";
-
-// export API handler
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: createContext,
-});
